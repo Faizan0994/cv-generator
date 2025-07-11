22072c24532f0eaf3a58d12f8719e68ee768a420
Faizan <faizanamjad575@gmail.com>
2025-07-11 14:40:26 +0500

    create a header

---
+++ src/components/layout.jsx
@@ -0,0 +1,13 @@
+import '../styles/layout.css';
+
+function Header({ children }) {
+    return (
+        <>
+            <header>
+                <h1>{children}</h1>
+            </header>
+        </>
+    );
+}
+
+export default Header;
